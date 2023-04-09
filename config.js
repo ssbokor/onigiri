9bfb27c7b9e4c3062c3bc9f2ac73e0fe4e6d0c2a
ssbokor <128565585+ssbokor@users.noreply.github.com>
2023-04-09 16:49:45 +1000

    Added files

---
+++ config.js
@@ -0,0 +1,14 @@
+const CONFIG = {
+// Autochange
+	autoChangeTheme: false,
+
+// Autochange by OS
+	changeThemeByOS: true,
+
+// Autochange by hour options (24hrs format, string must be in: hh:mm)
+	changeThemeByHour: false,
+	hourDarkThemeActive: '18:30',
+	hourDarkThemeInactive: '07:00',
+};
+
+// From https://migueravila.github.io/Bento/
